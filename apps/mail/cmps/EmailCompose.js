1aa3655d1255f645e3047b484ef858aa44842ea1
Dan Cohen <din1cohen1@gmail.com>
2023-03-02 11:14:33 +0200

    add

--- apps/mail/cmps/EmailCompose.js
+++ apps/mail/cmps/EmailCompose.js
@@ -5,21 +5,30 @@ export default {
             <header class="compose-header">
                 <h3>New Message</h3>
                 <div className="compose-buttons">
-                    <button class="minimize"><i class="fa-regular fa-window-minimize"></i></button>
-                    <button class="full-screen"><i class="fa-solid fa-up-right-and-down-left-from-center"></i></button>
-                    <button class="close" @click="close"><i class="fa-solid fa-xmark"></i></button>
+                    <button class="close-compose" @click="close"><i class="fa-solid fa-xmark"></i></button>
                 </div>
             </header>
 
             <main class="compose-text">
-                <input class="recipient" type="text" placeholder="Recipient"/>
-                <input class="mail-subject" type="text" placeholder="Subject"/>
-                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <input  type="text" placeholder="Recipient"/>
+                <input  type="text" placeholder="Subject"/>
+                <textarea name="" id="" cols="49" rows="17"></textarea>
             </main>
 
-            <section class="send-mail flex justify-between">
+            <section class="compose-send">
                 <button className="send">send</button>
                 <button className="delete"><i class="fa-regular fa-trash-can"></i></button>
             </section>
         </section>
-        `,}
+        `,
+        data(){
+            return {
+                
+            }
+        },
+        methods: {
+            close() {
+                this.$emit('close')
+            }
+        }
+    }
