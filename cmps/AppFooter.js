7e5ee0557a8ff0ed290983a08265337dee23a7f2
Teicher Ori <teicheror@mta.ac.il>
2023-03-03 16:19:24 +0200

    homepage design

--- cmps/AppFooter.js
+++ cmps/AppFooter.js
@@ -1,7 +1,7 @@
 export default {
-	template: `
+    template: `
         <footer class="app-footer">
-            <p>&copy; Coffeerights</p>
+            <p>&copy;All Rights Reserved</p>
         </footer>
     `,
 }
--- views/HomePage.js
+++ views/HomePage.js
@@ -1,7 +1,22 @@
 export default {
-	template: `
+    template: `
         <section class="home-page">
-            <h1>Home sweet home</h1>
+            <h1>Home sweet home!</h1>
+            <div class="home-menu">
+                <div>
+                    <img src="../../../img/gmail.logo.png">
+                    <h3>Get Your Own Email!</h3>
+                </div>
+                <div>
+                    <img src="../../../img/google.keep.logo.png">
+                    <h3>Keep Your Own Notes!</h3></h3>
+                </div>
+                <div>
+                    <img src="../../../img/books.logo.png">
+                    <h3>Our Books</h3>
+                </div>
+            </div>
+            </div>
         </section>
     `,
 }
